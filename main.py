547fb3a1dbe3944b9b55bad033794027370b5f29
Manuel Idrobo <idrobodev@gmail.com>
2025-10-05 09:55:29 -0500

    Update CORS configuration to allow all origins for better compatibility

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3001"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
